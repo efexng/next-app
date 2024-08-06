dddf8bda5b14a28d510fc8a2d666c4e5a57dcfda
efexng <efeapoki2020@gmail.com>
2024-08-06 23:58:19 +0100

    input

---
+++ app/components/AddTocart.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const AddTocart = () => {
+  return (
+    <div>
+        <button className='btn btn-primary' onClick={ () => console.log('Click')}>Add to cart</button>
+    </div>
+  )
+}
+
+export default AddTocart
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './ProductCard.module.css'
+import AddTocart from './AddTocart';
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddTocart />
+    </div>
+  )
+}
+
+export default ProductCard
